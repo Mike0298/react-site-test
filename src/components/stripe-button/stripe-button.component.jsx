801411c424ba170e2ae80143f8206d1664a729d1
Mike0298 <38065113+Mike0298@users.noreply.github.com>
2020-07-25 18:48:30 +1000

    add stripe payment(test)

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    "pk_test_51H8iDWCq1aDu6UdsMLD5o32Is9wYvSuzYXIBwx90pI236gkVuGiTgF1uAlzleVuQ3Qg3DOU5HfXaQAodfpzKDs7500N00HTqDs";
+
+  const onToken = (token) => {
+    console.log(token);
+    alert("Payment Successful");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="CRWN Clothing Ltd"
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
